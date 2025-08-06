001158faf87cc1b5df6cb635957c554d86590c04
maduasoares <eduarda.aleixo@escolar.ifrn.edu.br>
2025-08-06 17:53:06 -0300

    ...

---
+++ poo2b/Assets/Scripts/ControleDeAudio.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class ControleDeAudio : MonoBehaviour
+{
+    public int volume;
+    public int volumeSFX;
+    public bool musica; 
+    
+    public Slider volumeSlider;
+        public Slider volumeSEXSlider; 
+            public toggle toggleMusica
+            
+            
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
